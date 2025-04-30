55690b2f6ffd498a325624993bb33a11db153ee8
PritamSam <pritamsamantabond007@gmail.com>
2025-04-30 16:11:12 +0530

    Initial commit

---
+++ Program8.js
@@ -0,0 +1,9 @@
+// Objects  - they are the collection of properties and can be defined in key value pairs
+
+let person = {
+    firstname : "Pritam",
+    lastname : "Samanta"
+}
+
+console.log(person.firstname)
+console.log(person.lastname)
