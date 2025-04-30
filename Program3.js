bb8353d6b88ead5a1cf18e5d36ab5450f6f07e76
PritamSam <47378663+PritamSam@users.noreply.github.com>
2025-04-30 18:30:17 +0530

    Create Program3.js

---
+++ Program3.js
@@ -0,0 +1,14 @@
+// if else operation
+
+const flag=true
+
+if(!flag)
+{
+    console.log(flag)
+    console.log("Condition satisfied")
+}
+else
+{
+    console.log(flag)
+    console.log("Condition not satisfied")
+}
