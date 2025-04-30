f3bd993260dc3c98d4af70c9d5ab7b0322ed85eb
PritamSam <47378663+PritamSam@users.noreply.github.com>
2025-04-30 18:32:12 +0530

    Create Program6.js

---
+++ Program6.js
@@ -0,0 +1,34 @@
+// Functions
+// var - Access in global level not in function level
+// let - Access in global level and in block level{}
+// const - cannot be reintialize and value will remian same throughout the program
+
+let greet = "Pritam"
+
+if (1==1)
+{
+   let greet = "Nishant"
+}
+// Normal function syntax in js
+function add(a,b)
+{
+ return a+b
+}
+
+let sum = add(2,3)
+console.log(sum)
+console.log(greet)
+
+// Anonymous fucntions - functions which do not have name and can be assigned to variables
+//Type 1 -
+let sum1 = function (c,d)
+{
+  return c+d
+}
+
+console.log(sum1(4,4))
+
+//Type 2 -
+let sum2 = (c,d)=>c+d
+
+console.log(sum2(2,3)) 
