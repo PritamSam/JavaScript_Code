395980a5da38e4823fa08d26d95d2baa2689da70
PritamSam <47378663+PritamSam@users.noreply.github.com>
2025-04-30 18:27:25 +0530

    Create Program1.js

---
+++ Program1.js
@@ -0,0 +1,28 @@
+/* In order to execute js program, we write "node programname.js" in terminal
+ since node js is installed in system, hence we are using node JS to run commands */
+// Command to execute print statement
+console.log("Hello Pritam")
+// Javascript variables are loosely type, meaning no sepearte data type is needed to decalre vairables
+  let a=4
+  console.log(a)
+// (var)-[ES5] engine (let,const)-[ES6 and higher versions]
+// to print the data type of variable
+  console.log(typeof(a))
+
+  let b= 2.3
+  console.log(typeof(b))
+
+  let c= "Pritam"
+  console.log(typeof(c))
+
+  let d=true
+  console.log(typeof(d))
+  // In js, we have 6 types of variables :- number,string,boolean,NULL,undefine
+
+  /*Diff between let and var
+  we cannot redeclare variables with let keyword but possible with var
+  we cannot redeclare vairables with const keyword as the value will remian constant throughout the program
+  */
+
+  // Note - "!" operator can only be used in boolean data type to reverse the value
+  console.log(!d) // oputut - false
