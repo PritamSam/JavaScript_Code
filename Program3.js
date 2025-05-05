34c8b808a76ea42f888c6c33691ba91b2f3ebacb
PritamSam <pritamsamantabond007@gmail.com>
2025-05-05 18:16:46 +0530

    removed additioanl lines from code

--- Program3.js
+++ Program3.js
@@ -11,12 +11,6 @@ else
 {
     console.log(flag)
     console.log("Condition not satisfied")
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 4652a247fd19c720484ecf9b3395e3fdb585c6f2
-=======
-}
->>>>>>> master
+
